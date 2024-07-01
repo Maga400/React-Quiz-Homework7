b8762e6d9c6e4b8c1734eb1b5f69efaba7ff2f47
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-07-01 16:32:36 +0400

    first

---
+++ src/Data.js
@@ -0,0 +1,128 @@
+export const questions = 
+[
+    {
+        id:1,
+        question:"Azerbaycanin paytaxti haradir?",
+        answer1:"Baku",
+        answer2:"Quba",
+        answer3:"Samaxi",
+        trueAnswer:"Baku",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:2,
+        question:"Ronaldo hansi klubda oynayir?",
+        answer1:"Real Madrid",
+        answer2:"Al Nassr FC",
+        answer3:"Barcelona",
+        trueAnswer:"Al Nassr FC",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:3,
+        question:"Quba neyi ile meshurdur?",
+        answer1:"Alma",
+        answer2:"Heyva",
+        answer3:"Gilas",
+        trueAnswer:"Alma",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:4,
+        question:"Step It Academy hansi ilden etibaren movcuddur?",
+        answer1:"2020",
+        answer2:"2015",
+        answer3:"1999",
+        trueAnswer:"1999",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:5,
+        question:"Step Nece ildirki Azerbaycanda movcuddur?",
+        answer1:"8",
+        answer2:"2",
+        answer3:"3",
+        trueAnswer:"8",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+
+    },
+    {
+        id:6,
+        question:"C.Ronaldonun ilk oynadigi klub",
+        answer1:"Sporting Lisbon",
+        answer2:"Porto",
+        answer3:"Inter",
+        trueAnswer:"Sporting Lisbon",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:7,
+        question:"Azerbaycan tarixinin en boyuk dovleti hansidir?",
+        answer1:"Manna",
+        answer2:"Sefeviler",
+        answer3:"Sirvansahlar",
+        trueAnswer:"Sirvansahlar",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:8,
+        question:"Chevrolet aid olan masin hansidir?",
+        answer1:"Sonata",
+        answer2:"Cruze",
+        answer3:"Astra",
+        trueAnswer:"Cruze",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:9,
+        question:"Esger sayisina gore qedim en boyuk turk xalqi hansidir?",
+        answer1:"Selcuklular",
+        answer2:"Gokturkler",
+        answer3:"Osmanli",
+        trueAnswer:"Gokturkler",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    },
+    {
+        id:10,
+        question:"Dunyanin en bahali masini neceyedir?",
+        answer1:"18,7 milyon dollar",
+        answer2:"10,2 milyon dollar",
+        answer3:"15 milyon dollar",
+        trueAnswer:"18,7 milyon dollar",
+        bgColor:"grey",
+        color:"black",
+        e:"",
+        isCheck:false
+    }
+
+]
